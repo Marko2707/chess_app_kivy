f4117b215fb3c8ff3d4b4856bcb595f4108e1bf4
FrederikStb <41231213+FredStb@users.noreply.github.com>
2025-03-03 22:16:21 +0100

    open default browser with lichess board editor

--- chessapp/screens/main_screen.py
+++ chessapp/screens/main_screen.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import webbrowser
 
 from kivy.uix.screenmanager import Screen
 
@@ -10,4 +11,6 @@ load_kv_path(main_screen_kv)
 
 
 class MainScreen(Screen):
-    pass
+    def openWebsite(self, text):
+        webbrowser.open('https://lichess.org/editor/rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR_w_KQkq_-_0_1?color=white')
+        
